5eefe413ffe7fb9d52b3bcd3f31f9d5c80161355
Nezametdinov E. Ildus <neil.log@gmail.com>
2024-06-14 15:50:00 +0300

    Upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# DESCRIPTION
+This repository contains a template for building C++ applications.
+
+# LICENSE
+Copyright Nezametdinov E. Ildus 2024.
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file LICENSE_1_0.txt or copy at
+https://www.boost.org/LICENSE_1_0.txt)
---
+++ src/main.cc
@@ -0,0 +1,19 @@
+// Copyright Nezametdinov E. Ildus 2024.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// https://www.boost.org/LICENSE_1_0.txt)
+//
+#include <everything>
+
+import rose.filesystem;
+import rose.random;
+import rose.span;
+
+////////////////////////////////////////////////////////////////////////////////
+// Program entry point.
+////////////////////////////////////////////////////////////////////////////////
+
+int
+main() {
+    return EXIT_SUCCESS;
+}
